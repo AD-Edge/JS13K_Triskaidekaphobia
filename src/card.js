385e2242978d8cb4fbb7c0573854d390b1bb124e
AD-Edge <alex.delderfield@gmail.com>
2024-08-15 10:26:52 +0930

    mini cards for debugging, temp sprite files

---
+++ src/card.js
@@ -0,0 +1,16 @@
+class card {
+    constructor(numID, pos, type) {
+        this.numID = numID;
+        this.pos = pos;
+        this.type = type;
+
+        // this.image = new Image();
+        // this.image.src = 'src/textures/card_temp_' + this.typeID + '.png';
+    }
+    
+    render() {
+        
+    }
+}
+
+export default card;
