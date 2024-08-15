b0768282be72bf46f77eceedce47fec7d81d95a5
AD-Edge <alex.delderfield@gmail.com>
2024-08-15 12:28:03 +0930

    can now click and drag null card

--- src/card.js
+++ src/card.js
@@ -1,19 +1,64 @@
 class card {
     constructor(numID, pos, type) {
         this.numID = numID;
-        this.pos = pos;
+        this.pos = {
+            x: pos.x,
+            y: pos.y
+        };
         this.type = type;
 
         this.image = new Image();
+        this.hoverImage = new Image();
         // this.image.src = './img/card_temp_' + this.typeID + '.png';
         this.image.src = './img/mNUL.png';
+        this.hoverImage.src = './img/mHOV.png';
+        
+        this.isHovered = false;
+        this.isHeld = false;
     }
     
     render(cx, w, h) {
+        const img = this.isHeld ? this.hoverImage : this.image;
+
         if(this.type == null) {
-            context.drawImage(this.image, w * 0.5, h * 0.5, w/10, w/10);
+            cx.drawImage(img, w * this.pos.x, h * this.pos.y, h/10, w/10);
         }
+
+        if(this.isHovered) {
+            cx.fillStyle = '#FF000080';
+            cx.fillRect(w*this.pos.x, h * this.pos.y, h/10, w/10);
+        } else {
+            // cx.fillStyle = '#FFFFFF00';
+        }
+
     }
+
+    //Bounding box check for hover
+    checkHover(mX, mY, w, h) {
+        const width = h/10;
+        const height = w/10;
+        
+        console.log("checking hover");
+        if(this.isHeld) {
+            this.pos.x = (mX/w)-(width/w/2);
+            this.pos.y = (mY/h)-(height/h/2);
+        }
+
+        return (mX >= w*this.pos.x && mX <= (w*this.pos.x) + width 
+        && mY >= h*this.pos.y && mY <= (h*this.pos.y) + height);
+    }
+
+    checkClick(clk) {
+        if(clk) {
+            if(this.isHovered) {
+                this.isHeld = true;
+            }
+        } else {
+            this.isHeld = false;
+        }
+        console.log("click: " + clk);
+    }
+
 }
 
 export default card;
--- src/main.js
+++ src/main.js
@@ -14,10 +14,14 @@ var height = 0;
 var rng = null;
 var seed = null;
 var complex = true;
+var rand = null;
+
+var mouseX = -999;
+var mouseY = -999;
 
 const flashDuration = 200;
 
-const cardSlot1 = new card(0, 0, 0);
+const cardSlot1 = new card(0, {x: 0.45, y: 0.45}, null);
 
 var playerCardHand = [
     cardSlot1,
@@ -35,6 +39,7 @@ window.onload = function() {
     //canvas setup
     canvas = document.getElementById('canvasMain');
     ctx = canvas.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
     width = canvas.clientWidth;
     height = canvas.clientHeight;
 
@@ -42,25 +47,6 @@ window.onload = function() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     canvas.style.outlineColor  = '#000000';
 
-    // setTimeout clears the white flash after the specified duration
-    setTimeout(() => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = '#111111';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        
-        // Draw initial content (if any)
-        drawTestContent(ctx);
-    }, flashDuration);
-}
-
-function drawTestContent() {
-    // Draw Test #1
-    ctx.globalAlpha = 0.8;
-    // [font style][font weight][font size][font face]
-    ctx.font = "normal bold 16px monospace";
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fillText("JS13K 2024", 0.04*width, 0.1*height);
-    
     //Different ways to use random function
     if(complex) {
         //Non deterministic 
@@ -74,10 +60,69 @@ function drawTestContent() {
     //rng test   
     //PRNG via ooflorent/example.js
     rng = createNumberGenerator(
-            createSeedFromString(seed)
-        );
+        createSeedFromString(seed)
+    );
     // let rand = 0;
-    var rand = generateNumber(rng, -10, 10);
+    rand = generateNumber(rng, -10, 10);
+
+    // Event listener to track mouse movement
+    canvas.addEventListener('pointermove', (e) => {
+        const rect = canvas.getBoundingClientRect();
+        mouseX = e.clientX - rect.left;
+        mouseY = e.clientY - rect.top;
+
+        // Check if the card is hovered
+        if (playerCardHand[0].checkHover(mouseX, mouseY, width, height)) {
+            playerCardHand[0].isHovered = true;
+        } else {
+            playerCardHand[0].isHovered = false;
+        }
+
+    });
+
+    canvas.addEventListener('pointerdown', (e) => {
+        // const rect = canvas.getBoundingClientRect();
+        // const mouseX = e.clientX - rect.left;
+        // const mouseY = e.clientY - rect.top;
+        // console.log("click");
+
+        playerCardHand[0].checkClick(true);
+        // drawBox(mouseX, mouseY, '#0000FFFF');
+        // Call a function to handle the click event
+        // handlePointerClick(mouseX, mouseY);
+    });
+    canvas.addEventListener('pointerup', (e) => {
+        // console.log("click");
+        playerCardHand[0].checkClick(false);
+    });
+
+    // setTimeout clears the white flash after the specified duration
+    setTimeout(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = '#111111';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        
+        // Draw initial content (if any)
+        renderScene();
+    }, flashDuration);
+}
+
+function drawBox(x, y, c) {
+    const boxSize = 20;
+    ctx.fillStyle = c;
+    ctx.fillRect(x - boxSize / 2, y - boxSize / 2, boxSize, boxSize);
+}
+
+function renderScene() {
+    ctx.clearRect(0, 0, width, height);
+
+    // Draw Test #1
+    ctx.globalAlpha = 0.8;
+    // [font style][font weight][font size][font face]
+    ctx.font = "normal bold 16px monospace";
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillText("JS13K 2024 - Day 2", 0.04*width, 0.1*height);
+    
     // Draw Test #2
     ctx.fillText("RNG TEST: " + rand, 0.04*width, 0.15*height);
     
@@ -87,4 +132,10 @@ function drawTestContent() {
 
     //test draw card
     playerCardHand[0].render(ctx, width, height);
+
+
+    drawBox(mouseX, mouseY, '#FF000080');
+
+    // Request next frame
+    requestAnimationFrame(renderScene);
 }
