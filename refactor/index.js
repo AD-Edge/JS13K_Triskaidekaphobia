6f775a443a1cb282e144e7be55c851db347d87b7
alex <alex.delderfield@gmail.com>
2024-08-27 19:50:13 +0930

    starting refactor

---
+++ refactor/index.js
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////
+// Main Game Class
+/////////////////////////////////////////////////////
+// import './style.css';
+
+var canvas, ctx;
+
+// App Setup
+window.onload = function() {
+    // canvas setup
+    canvas = document.getElementById('canvasMain');
+    ctx = canvas.getContext("2d");
+
+    console.log("Game Started");
+}
