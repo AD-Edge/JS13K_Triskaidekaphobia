b61561b3af1fb66a07862a75fbe2fc94cd564e03
alex <alex.delderfield@gmail.com>
2024-08-27 20:44:26 +0930

    canvas flips to landscape on mobile, size increase to 960x540

--- refactor/index.js
+++ refactor/index.js
@@ -4,12 +4,20 @@
 // import './style.css';
 
 var canvas, ctx;
+var width, height;
+var w2 = 720;
 
 // App Setup
 window.onload = function() {
     // canvas setup
     canvas = document.getElementById('canvasMain');
     ctx = canvas.getContext("2d");
+    width = canvas.clientWidth;
+    height = canvas.clientHeight;
 
     console.log("Game Started");
+
+    ctx.fillStyle = '#448';
+    ctx.fillRect(width*0.125, 0, w2, height);
+
 }
