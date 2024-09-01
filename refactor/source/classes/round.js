bde182724805b6ee3ff852b47ea9a0fea7fd1c83
AD-Edge <alex.delderfield@gmail.com>
2024-09-01 15:53:58 +0930

    adding round class, music setup class

---
+++ refactor/source/classes/round.js
@@ -0,0 +1,11 @@
+class round {
+    constructor(rID, score, diff, char) {
+        this.rID = rID, this.score = score, this.diff = diff, this.char = char;
+
+        this.completed = false;
+    }
+    
+    // Render Card
+    render() {
+    }
+}
